436fff5c831f321327ec55d772329cd46548ef30
dukaludic <dukaludic@gmail.com>
2022-02-01 21:49:50 +0100

    tasks

--- src/pages/tasks.js
+++ src/pages/tasks.js
@@ -3,7 +3,7 @@ import Sidebar from "../components/shared/sidebar";
 import { Container, Col, Row } from "react-bootstrap";
 import TasksList from "../components/parts/tasksList";
 
-import * as datahandler from '../helpers/dataHandler';
+import * as datahandler from "../helpers/dataHandler";
 
 function Tasks() {
   const [tasks, setTasks] = useState([]);
@@ -12,23 +12,24 @@ function Tasks() {
   const [inReview, setInReview] = useState([]);
   const [done, setDone] = useState([]);
 
-
   useEffect(() => {
-    (async() => {
-      const tasks = await this.datahandler('tasks')
-      setTasks(tasks)
-      
-      const todo = tasks.filter((task) => task.status === 'to_do');
-      const inProgress = tasks.filter((task) => task.status === 'in_progress');
-      const inReview = tasks.filter((task) => task.status === 'in_review');
-      const done = tasks.filter((task) => task.status === 'done');
+    (async () => {
+      const tasks = await datahandler.show("tasks");
+      setTasks(tasks);
+
+      const todo = tasks.filter((task) => task.status === "to_do");
+      const inProgress = tasks.filter((task) => task.status === "in_progress");
+      const inReview = tasks.filter((task) => task.status === "in_review");
+      const done = tasks.filter((task) => task.status === "done");
+
+      console.log(todo, inProgress, inReview, done);
 
       setTodo(todo);
       setInProgress(inProgress);
       setInReview(inReview);
       setDone(done);
-    })()
-  }, [])
+    })();
+  }, []);
 
   return (
     <div className="d-flex">
@@ -42,10 +43,10 @@ function Tasks() {
         <Row>
           <Col lg={12}>
             <TasksList
-            todo={todo}
-            inProgress={inProgress}
-            inReview={inReview}
-            done={done}
+              todo={todo}
+              inProgress={inProgress}
+              inReview={inReview}
+              done={done}
             />
           </Col>
         </Row>
